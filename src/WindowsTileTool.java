e603b7b4e2169120d03e16e2d6be0ae55fbccdca
Alexander Shearer <sheareraws@gmail.com>
2017-12-01 21:50:43 -0500

    reads start menu, creates simplified WindowsTile

--- src/WindowsTileTool.java
+++ src/WindowsTileTool.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+
 /**
  * Tool that allows user to customize Windows start menu tiles.
  *
@@ -5,9 +10,54 @@
  */
 public class WindowsTileTool {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+    private static ArrayList<WindowsTile> tiles;
 
+    public static void main(String[] args) throws IOException, ParseException {
+	loadTiles();
+    }
+
+    /**
+     * loads WindowsTiles from the start menu
+     */
+    private static void loadTiles() {
+	String drive = System.getenv("SystemDrive");
+	String startMenuPath = drive + "/ProgramData/Microsoft/Windows/Start Menu/Programs/";
+	File startMenu = new File(startMenuPath);
+	File[] shortcutFiles = getAllLinks(startMenu);
+	tiles = new ArrayList<WindowsTile>();
+	for (File f : shortcutFiles) {
+	    try {
+		WindowsShortcut s = new WindowsShortcut(f);
+		if (s.getRealFilename().startsWith(drive)) {
+		    WindowsTile t = new WindowsTile(f.getName(), s.getRealFilename());
+		    tiles.add(t);
+		}
+
+	    } catch (Exception e) {
+		// TODO: Process website shortcuts
+	    }
+	}
+    }
+
+    /**
+     * Finds all .lnk files within a directory and the sub directories
+     *
+     * @param directory
+     *            the root directory to be searched
+     * @return array of all .lnk Files
+     */
+    private static File[] getAllLinks(File directory) {
+	ArrayList<File> files = new ArrayList<File>();
+	for (File f : directory.listFiles()) {
+	    if (f.isFile() && f.getAbsolutePath().endsWith(".lnk")) {
+		files.add(f);
+	    } else if (f.isDirectory()) {
+		for (File subF : getAllLinks(f)) {
+		    files.add(subF);
+		}
+	    }
+	}
+	return files.toArray(new File[files.size()]);
     }
 
 }
